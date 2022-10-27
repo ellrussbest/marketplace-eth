0253a4b1c60aeb9881bc51444c3999a552de629e
Russell Best <ellrussbest@gmail.com>
2022-10-27 11:00:00 +0300

    order modal & form

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -3,6 +3,9 @@ import { BaseLayout } from "@components/ui/layout";
 import { Walletbar } from "@components/ui/web3";
 import { getAllCourses } from "content/courses/fetcher";
 import { useAccount, useNetwork } from "@components/hooks/web3";
+import { Button, Modal } from "@components/ui/common";
+import { useState } from "react";
+import { OrderModal } from "@components/ui/order";
 
 export default function Marketplace({ courses }) {
   const { data: address } = useAccount();
@@ -13,6 +16,8 @@ export default function Marketplace({ courses }) {
     targetNetwork,
     hasFinishedFirstFetch,
   } = useNetwork();
+
+  const [selectedCourse, setSelectedCourse] = useState(null);
 
   return (
     <>
@@ -27,13 +32,32 @@ export default function Marketplace({ courses }) {
       </div>
       <CourseList courses={courses}>
         {(course) => {
-          return <CourseCard key={course.id} course={course} />;
+          return (
+            <CourseCard
+              key={course.id}
+              course={course}
+              Footer={() => (
+                <div className="mt-4">
+                  <Button
+                    onClick={() => {
+                      setSelectedCourse(course);
+                    }}
+                    variant="lightPurple"
+                  >
+                    Purchase
+                  </Button>
+                </div>
+              )}
+            />
+          );
         }}
       </CourseList>
-
-      {/* {courses.map((course) => (
-        <CourseCard course={course} key={course.id} />
-      ))} */}
+      {selectedCourse && (
+        <OrderModal
+          course={selectedCourse}
+          setSelectedCourse={setSelectedCourse}
+        />
+      )}
     </>
   );
 }
